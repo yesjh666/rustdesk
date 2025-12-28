539aef4ba59df2b5612a73232d77dd33367a9fb5
yesjh666 <154724043+yesjh666@users.noreply.github.com>
2025-12-28 16:51:07 +0800

    Change default URL from Rustdesk to custom URL

--- src/common.rs
+++ src/common.rs
@@ -1058,7 +1058,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://xg2.yesjh.com:21114".to_owned()
 }
 
 #[inline]
